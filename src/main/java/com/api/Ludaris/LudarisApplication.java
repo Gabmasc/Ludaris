812f2fe47051631f0b798dc0aac4f2c7edb0bdec
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-05-20 10:01:43 -0300

    Project Ludaris Started!
    Signed-off-by: Gabriel Mascarenhas <devgabrielprog@gmail.com>

---
+++ src/main/java/com/api/Ludaris/LudarisApplication.java
@@ -0,0 +1,13 @@
+package com.api.Ludaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudarisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudarisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/Ludaris/LudarisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.Ludaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LudarisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
