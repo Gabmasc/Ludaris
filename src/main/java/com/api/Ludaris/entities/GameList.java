f233a61cf9ee2396af1d11e793642d02b92630cc
Gabriel Mascarenhas <devgabrielprog@gmail.com>
2025-05-22 15:58:02 -0300

    ORM implementation Class

---
+++ src/main/java/com/api/Ludaris/entities/GameList.java
@@ -0,0 +1,55 @@
+package com.api.Ludaris.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity(name = "tb_gameList")
+public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public GameList() {
+    }
+
+    public GameList(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameList gameList = (GameList) o;
+        return Objects.equals(id, gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
